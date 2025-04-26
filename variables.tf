b41a8d7209cae68a0786652d75f82d6d5b35ac45
pythonjourney <hitechminds99@gmail.com>
2025-04-27 02:58:26 +0530

    added vars for vpc

--- variables.tf
+++ variables.tf
@@ -4,3 +4,11 @@ variable "cidr_block"{
     description = "cidr range for vpc"
     default = "10.0.0.0/16"
 }
+
+variable "tags" {
+
+
+    type = map(string)
+    description = "Tags for the vpc"
+    default = {}
+}
